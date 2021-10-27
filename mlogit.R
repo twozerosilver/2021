8a7f06916c3ecc7d3ee976e97e2eef07ac4a45ef
twozerosilver <paradise7917@gmail.com>
2021-10-27 22:07:19 +0900

    ##reflevel인식이 안됨

--- mlogit.R
+++ mlogit.R
@@ -16,10 +16,10 @@ getwd()
 chatData<-read.delim("Chat-Up Lines.dat", header = TRUE)
 chatData$Gender<-relevel(chatData$Gender, ref = 2)
 
-##-------데이터를 적절한 형식으로 바꾸기
+##-------데이터를 확인하기
 ##텍스트 문자열이 포함되어 있으므로 요인여부 확인하고, 필요 시 요인으로 변환하기
 ##해석을 고려하여 기저범주 설정하기: 현재는 Female이 기저범주. 여성에 대한 작업멘트의 성공 여부가 궁금하다면 기저범주는 male로 두어야 함
-##기존의 데이터를 mlogit에 적정한 형태로 바꾸기
+
 head(chatData)
 is.factor(chatData$Success)
 is.factor(chatData$Gender)
@@ -29,14 +29,32 @@ chatData$Gender=factor(chatData$Gender)
 str(chatData)
 chatData$Gender<-relevel(chatData$Gender,ref=2)
 
-###newDataframe<-mlogit.data(oldDataFrame, choice = "text", shape = "wide"/"long")
+
+##-------기존의 데이터를 mlogit에 적정한 형태로 바꾸기
+##newDataframe<-mlogit.data(oldDataFrame, choice = "text", shape = "wide"/"long")
+##long layout: 1행 1 관측치, wide layout: 1행 여러 데이터
+##각 행이 3가지 종속변수(Success)에 대응하여 분리됨
 mlChat<-mlogit.data(chatData, choice = "Success", shape="wide")
 head(mlChat)
 
 ##-------다항 로지스틱 회귀분석 모델 만들기
-chatModel <- mlogit(Success ~ 1 | Good_Mate + Funny + Gender + Sex + Gender:Sex +  Funny:Gender , data = mlChat, reflevel=3)
+##newDataframe<-mlogit.data(결과변수~예측변수, data = 데이터프레임, na.action = 결측값 처리방식, relevel = 결과 기저 범주 번호)
+##기존 lm이나 glm에 비해 "relevel"을 해야 한다는 특징
+##기저범주 설정하기: 무반응"No response/Walk off"
+##예측변수 결정하기: 기존 예측변수들 + 성적 농담과 성별의 상호작용 + 유머와 성별의 상호작용
+chatModel <- mlogit(Success ~ 1 | Good_Mate + Funny + Gender + Sex + Gender:Sex + Funny:Gender, data = mlChat, reflevel = 3)
 summary(chatModel)
 
+##-------회귀모형 해석하기
+chatBase <- mlogit(Success ~ 1, data = mlChat, relevel=3)
+summary(chatBase)
+
+##위 기저모형의 Log-Likelihood: Log-Likelihood: -1008 / chatModel은 Log-Likelihood: -868.74
+##둘의 차이는 139.26, 카이제곱 -2LL=278.52
+##Likelihood ratio test : chisq = 278.52 (p.value = < 2.22e-16) => 전체 모형의 가능도비에 따라 이러한 변화는 유의하다
+##즉, 기저모형보다 chatModel이 더 적합하다(많이 설명한다)
+
+exp(chatModel$coefficients)
 data.frame(exp(chatModel$coefficients))
 exp(confint(chatModel))
 
