5094f5c538b68d569003492ea0f73241920c9b42
twozerosilver <paradise7917@gmail.com>
2021-11-03 22:48:58 +0900

    마지막 시도

--- mlogit_3values.R
+++ mlogit_3values.R
@@ -7,14 +7,12 @@
 install.packages("car")
 install.packages("mlogit")
 install.packages("nnet")
-install.packages("ggplot2")
 install.packages("dfidx")
 
 library(car)
 library(mlogit)
-library(dfidx)
 library(nnet)
-library(ggplot2)
+library(dfidx)
 
 ##-------디렉토리 설정하기
 getwd()
@@ -44,7 +42,8 @@ levels(chatData$Gender)
 chatData$Gender<-relevel(chatData$Gender,"Male")
 chatData$Success<-relevel(chatData$Success, "No response/Walk Off")
 
-chatData$Success<-factor(chatData$Success, levels = c("No response/Walk Off", "Get Phone Number", "Go On A Date"))
+chatData$Success<-factor(chatData$Success, levels = c("No response/Walk Off", 
+                                         "Get Phone Number", "Go On A Date"))
 levels(chatData$Success)
 chatData$Gender<-factor(chatData$Gender, levels = c("Male", "Female"))
 levels(chatData$Gender)
@@ -58,57 +57,63 @@ head(mlChat, 10)
 
 ##-------다항 로지스틱 회귀분석 모델 만들기
 ##newDataframe<-mlogit.data(결과변수~예측변수, data = 데이터프레임, na.action = 결측값 처리방식, reflevel = 결과 참조 범주 번호)
-##newDataframe<-multinom(결과변수~예측변수, data = 본 데이터)
 ##기존 lm이나 glm에 비해 "reflevel"을 해야 한다는 특징
 ##기저범주 설정하기: No response/Walk off
 ##예측변수 결정하기: Good_Mate, Funny, Sexual
 
-chatModel <- mlogit(Success ~ 1 | Good_Mate + Funny + Sexual + Gender, data = mlChat, reflevel = "No response/Walk Off")
-chatModel2 <- multinom(Success ~ Good_Mate + Funny + Sexual + Gender, data = chatData)
+chatModel <- mlogit(Success ~ 1 | Good_Mate + Funny + Sexual + Gender, 
+                    data = mlChat, reflevel = "No response/Walk Off")
 summary(chatModel)
-summary(chatModel2)
 
 ##-------회귀모형의 설명력 확인하기
-##-------우도(LL) 통계량
-##우도 통계량은 모형이 적합된 후에도 설명되지 않은 정보의 양
-##우도 통계량 값이 크다 => 설명되지 않은 관측이 많다
-
+##Log-Likelihood에서 X^=2LL(새모형)-2LL(기본모형)
+##이탈도는 카이제곱 분포를 따르므로 유의성을 계산하기 쉬움
 chatBase <- mlogit(Success ~ 1, data = mlChat, reflevel = "No response/Walk Off")
 summary(chatBase)
-
-##-------이탈도 통계량
-##-------로지스틱 회귀모형 이탈도 = -2LL
-##-------우도비: X^=2LL(생성 모형)-2LL(기저 상태)
-##로지스틱 회귀모형을 기저 상태와 비교
-##이탈도는 카이제곱 분포를 따르므로 유의성을 계산하기 쉬움
 
 ##chatBase의 Log-Likelihood: -1008 / chatModel은 Log-Likelihood: -892.02
 1008-892.02
 (1008-892.02)*-2
-##둘의 차이는 115.98, 카이제곱 -2LL=231.96
 ##Likelihood ratio test : chisq = 231.95 (p.value = < 2.22e-16)
 ##전체 모형의 가능도비에 따라 이러한 변화는 유의하다
 ##chatBase 보다 chatModel이 더 적합하다(많이 설명한다)
+##McFadden R^ / pseudo R^ (유사결정계수)
+##유사결정계수가 1에 가까운 값을 가질수록 로지스틱 회귀모형의 적합도는 높다고 평가 
+##유사결정계수가 0.2보다 큰 값을 갖게 되면 모형은 적합도가 비교적 높은 것으로 판단
 
+lrtest(chatModel, chatBase)
+waldtest(chatModel,chatBase)
+
+##-------[참고] nnet_multinom 활용
+##다항 로짓스틱 회귀분석 모델 만들기
+##newDataframe<-multinom(결과변수~예측변수, data = 본 데이터)
+chatModel2 <- multinom(Success ~ 
+                         Good_Mate + Funny + Sexual + Gender, data = chatData)
+summary(chatModel2)
+exp(coef(chatModel2))
+##변수의 유의성 검정(Z검정)
+z <- summary(chatModel2)$coefficients/summary(chatModel2)$standard.errors
+z
+p <- (1 - pnorm(abs(z), 0, 1)) * 2
+p
+##성별에 따른 예측값의 변화
+FtoM <- data.frame(Gender = c("Female", "Male"), 
+                   Sexual = mean(chatData$Sexual), 
+                   Good_Mate = mean(chatData$Good_Mate), 
+                   Funny = mean(chatData$Funny))
+FtoM
+predict(chatModel2, newdata = FtoM, "probs")
 
 ##-------전화번호를 얻을 확률은 어떻게 변화할까
 exp(chatModel$coefficients)
 data.frame(exp(chatModel$coefficients))
 exp(confint(chatModel))
 
-exp(coef(chatModel2))
-
-##-------변수의 유의성 검정(Z검정)
-z <- summary(chatModel2)$coefficients/summary(chatModel2)$standard.errors
-z
-p <- (1 - pnorm(abs(z), 0, 1)) * 2
-p
-
 ##-------회귀모형 결과 해석하기
 ##-------3. Good_Mate:Get Phone Number
 ##no response와 get phone number를 유의미하게 예측(b=0.13, p<0.05)
 exp(0.13)
-##Odds Ratio는 Good_Mate 점수가 1점 올라갈 때 무응답에 비해 전화번호를 얻을 확률이 1.14 높아짐
+##Odds Ratio는 Good_Mate 1점 올라갈 때 무응답에 비해 전화번호 얻을 확률이 1.14 높아짐
 ##-------5. Funny:Get Phone Number
 ##no response와 get phone number를 유의미하게 예측(b=0.49, p<0.001)
 exp(0.49)
@@ -117,14 +122,30 @@ exp(0.49)
 ##no response와 get phone number를 유의미하게 예측(b=-1.99, p<0.001)
 exp(-1.99)
 ##Odds Ratio는 남성(0)보다 여성(1)일 때 전화번호를 얻을 확률이 0.14 정도
+##여성보다 남성일 때 전화번호를 얻을 확률이 614.29% 증가
 
-##------종속변수 확률 구하기
+##------발생 확률 구하기
+head(fitted(chatModel, outcome = FALSE))
 apply(fitted(chatModel, outcome = FALSE), 2, mean)
+predict(mlChat, newdata=test1 , type="response")
+test1 <- data.frame(Gender = c("Female", "Male"), 
+                   Sexual = mean(chatData$Sexual), 
+                   Good_Mate = mean(chatData$Good_Mate), 
+                   Funny = mean(chatData$Funny))
+test1.pre <- predict(mlChat, newdata = test1, type = "response")
+predic
 
-##------성별에 따른 예측값의 변화(나머지는 평균일 때)
-FtoM <- data.frame(Gender = c("Female", "Male"), 
-                    Sexual = mean(chatData$Sexual), 
-                    Good_Mate = mean(chatData$Good_Mate), 
-                    Funny = mean(chatData$Funny))
+##------mlogit: 조건별 확률 구하기
+newdata1 <- with(mydata, data.frame(gre = mean(gre), gpa = mean(gpa), 
+                                    rank = factor(1:4)))
+newdata1$rankP <- predict(mylogit, newdata = newdata1, type = "response")
+
+FtoM <- with(chatData, data.frame(Gender = c("Female", "Male"), 
+                                  Sexual = mean(Sexual), 
+                                  Good_Mate = mean(Good_Mate), 
+                                  Funny = mean(Funny)))
 FtoM
-predict(chatModel2, newdata = FtoM, "probs")
+FtoM <- predict(chatModel, newdata = FtoM, type = "Success")
+
+
+
